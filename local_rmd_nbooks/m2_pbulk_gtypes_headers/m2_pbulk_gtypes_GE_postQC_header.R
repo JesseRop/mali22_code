50b489f6a4c10e4a1de8a9899f987e2d6c8c730f
Jesse Rop <rop.jesse@gmail.com>
2025-05-20 13:27:09 +0100

    headers that are replaced for various notebooks including 'm2_strain_postQC_decon_IBD.Rmd', 'm2_strain_postQC_decon_IBD_1vcf.Rmd', 'm2_strain_deconv_refnd_all.Rmd', 'm2_strain_deconv_refnd_all_1vcf.Rmd' which then stay fixed but can operate on different set of samples and parameters such as different mapping strategies and Ks as specified in the headers

---
+++ local_rmd_nbooks/m2_pbulk_gtypes_headers/m2_pbulk_gtypes_GE_postQC_header.R
@@ -0,0 +1,69 @@
+
+
+## Sample names with more than one strain
+# sample_name_nms <- c("MSC50_SLO", "MSC41", "MSC51", "MSC25") %>% str_sort(numeric = T)
+sample_name_nms <- c("MSC33", "MSC48","MSC49", "MSC55", "MSC60", "MSC66", "MSC38", "MSC40", "MSC50_MACS", "MSC53", "MSC57", "MSC24", "MSC37", "MSC39", "MSC45", "MSC50_SLO", "MSC54", "MSC67", "MSC68", "MSC70", "MSC41", "MSC51", "MSC25") %>% sort()
+
+
+# Get the optimum QCd K
+opt_narrow_clusters_nms <-  list(
+  "MSC33" = c(7:9),
+  "MSC48" = c(3:5),
+  "MSC49" = c(5:7),
+  "MSC55" = c(5:7),
+  "MSC60" = c(6:8),
+  "MSC66" = c(7:9),
+  "MSC38" = c(5:7),
+  "MSC40" = c(7:9),
+  "MSC50_MACS" = c(2:4),
+  "MSC53" = c(2),
+  "MSC57" = c(2),
+  "MSC24" = c(2),
+  "MSC37" = c(5:7),
+  "MSC39" = c(4:6),
+  "MSC45" = c(2),
+  "MSC50_SLO" = c(2:4),
+  "MSC54" = c(2),
+  "MSC67" = c(2),
+  "MSC68" = c(2:4),
+  "MSC70" = c(2),
+  "MSC41" = c(3:5),
+  "MSC51" = c(2),
+  "MSC25" = c(2:3)
+) %>% .[sample_name_nms]
+# optimum_k = opt_narrow_clusters_nms
+
+sample_name_k <- map2(sample_name_nms,
+                      opt_narrow_clusters_nms,
+                      ~ rep(.x, each = length(.y))) %>% unlist()
+
+## sorted donor_opt_cluster names
+sorted_names <- imap(opt_narrow_clusters_nms, ~ paste0(.y, "_", .x)) %>% unlist(., recursive =
+                                                                                  F, use.names = F)
+
+optimum_k <- opt_narrow_clusters_nms %>% unlist() %>% set_names(sorted_names)
+
+# Get the pseudobulk groups/categories
+grps <- c('stage_afm_strain')
+
+##Alignment incorporation
+algn_nms = c('minmap')
+algn_nm = 'minmap'
+
+# Directory to read pseudobulk genotype results from
+# gtyp_step = "pbulk_gtypes_preQC"
+gtyp_step = "pbulk_gtypes_GE_postQC"
+
+sample_set <- "Mali2"
+
+species <- "Pf"
+mt_chrom_nm <- "Pf3D7_MIT_v3"
+
+## NB-DONT DELETE BELOW- RUN ONCE Get genome size like this cut -f1,2 fasta.fai > genome.size
+system("cut -f1,2 /lustre/scratch126/tol/teams/lawniczak/projects/malaria_single_cell/mali_field_runs/2022/data/references_for_souporcell/PlasmoDB-66_Pfalciparum3D7_Genome.fasta.fai > /lustre/scratch126/tol/teams/lawniczak/users/jr35/Pf3D7_genomes_gtfs_indexs/Pdb66_Genome.size")
+
+chrm_sz <- "../../Pf3D7_genomes_gtfs_indexs/Pdb66_Genome.size"
+chrm_prfx <- "Pf3D7_0|Pf3D7_|_v3"
+
+## Directory to save barcodes for the next iteration of pseudobulk genotyping
+sv_dir = "pbulk_gtypes_postQC_cln"
---
+++ local_rmd_nbooks/m2_pbulk_gtypes_headers/m2_pbulk_gtypes_postQC_cln_header.R
@@ -0,0 +1,69 @@
+
+## Sample names with more than one strain
+# sample_name_nms <- c("MSC33","MSC37","MSC38","MSC39","MSC40", "MSC48", "MSC49", "MSC55", "MSC60", "MSC66", "MSC68", "MSC50_SLO", "MSC25", "MSC41") %>% sort()
+sample_name_nms <- c("MSC33", "MSC48","MSC49", "MSC55", "MSC60", "MSC66", "MSC38", "MSC40", "MSC50_MACS", "MSC53", "MSC57", "MSC24", "MSC37", "MSC39", "MSC45", "MSC50_SLO", "MSC54", "MSC67", "MSC68", "MSC70", "MSC41", "MSC51", "MSC25") %>% sort()
+
+
+
+# Get the optimum QCd K
+opt_narrow_clusters_nms <-  list(
+  "MSC33" = c(8),
+  "MSC37" = c(5),
+  "MSC38" = c(4),
+  "MSC39" = c(4),
+  "MSC40" = c(8),
+  "MSC48" = c(4),
+  "MSC49" = c(6),
+  "MSC55" = c(6),
+  "MSC60" = c(7),
+  "MSC66" = c(8),
+  "MSC68" = c(3),
+  "MSC50_SLO" = c(2),
+  "MSC50_MACS" = c(1),
+  "MSC53" = c(1),
+  "MSC57" = c(1),
+  "MSC24" = c(1),
+  "MSC45" = c(1),
+  "MSC54" = c(1),
+  "MSC67" = c(1),
+  "MSC70" = c(1),
+  "MSC41" = c(3),
+  "MSC51" = c(1),
+  "MSC25" = c(2)
+) %>% .[sample_name_nms]
+
+
+sample_name_k <- map2(sample_name_nms,
+                      opt_narrow_clusters_nms,
+                      ~ rep(.x, each = length(.y))) %>% unlist()
+
+## sorted donor_opt_cluster names
+sorted_names <- imap(opt_narrow_clusters_nms, ~ paste0(.y, "_", .x)) %>% unlist(., recursive =
+                                                                                  F, use.names = F)
+
+optimum_k <- opt_narrow_clusters_nms %>% unlist() %>% set_names(sorted_names)
+
+# Get the pseudobulk groups/categories
+grps <- c('strain', 'stage_ag_strain')
+
+##Alignment incorporation
+# algn_nms = c('minmap', 'hsat')
+algn_nms = c('minmap')
+algn_nm = 'minmap'
+
+# Directory to read pseudobulk genotype results from
+gtyp_step = "pbulk_gtypes_preQC_cln"
+
+sample_set <- "Mali2"
+
+species <- "Pf"
+mt_chrom_nm <- "Pf3D7_MIT_v3"
+
+## NB-DONT DELETE BELOW- RUN ONCE Get genome size like this cut -f1,2 fasta.fai > genome.size
+# system("cut -f1,2 /lustre/scratch126/tol/teams/lawniczak/projects/malaria_single_cell/mali_field_runs/2022/data/references_for_souporcell/PlasmoDB-66_Pfalciparum3D7_Genome.fasta.fai > /lustre/scratch126/tol/teams/lawniczak/users/jr35/Pf3D7_genomes_gtfs_indexs/Pdb66_Genome.size")
+
+chrm_sz <- "../../Pf3D7_genomes_gtfs_indexs/Pdb66_Genome.size"
+chrm_prfx <- "Pf3D7_0|Pf3D7_|_v3"
+
+## Directory to save barcodes for the next iteration of pseudobulk genotyping
+sv_dir = "pbulk_gtypes_preQC_cln_final"
